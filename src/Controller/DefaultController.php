16d0cbf426785b3dc4145621c595859482ba1595
Arveez <rv@arveez-news.com>
2018-02-15 17:56:48 +0100

    db ready

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,20 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use  Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-class DefaultController extends AbstractController
+class DefaultController extends Controller
 {
     public function index()
     {
-        $url = $this->generateUrl('default', [
-            'page' => 2,
-            'visibility' => 'hidden'
-        ]);
-        $absoluteUrl = $this->generateUrl('default', [], UrlGeneratorInterface::ABSOLUTE_URL);
-
         return new Response($this->render('template.html.twig', [
             'initialArticles' => json_encode(['eau', 'pain', 'fromage', 'cafe'])
         ]));
@@ -24,7 +20,14 @@ class DefaultController extends AbstractController
     }
     public function returnTest() {
 
-        return new Response ("return !!");
+        return new Response ("return !! ". $this->container->getParameter('boota'));
 
+    }
+
+    /**
+     * @Route("/annotation-route", name="annotaion_route")
+     */
+    public function annotaitonRouting () {
+        return new Response("annotation route in controller");
     }
 }
