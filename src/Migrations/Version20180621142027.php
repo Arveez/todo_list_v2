a405aa2c58cb3d68a07a1bb101bf1c8867fa4817
Arveez <rv@arveez-news.com>
2018-06-23 16:35:26 +0200

    post machine re-installation adjustments

--- src/Migrations/Version20180621142027.php
+++ src/Migrations/Version20180621142027.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180423172902 extends AbstractMigration
+class Version20180621142027 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
